7ccdf1288d9e15d66d77b566b601847f53e5d762
HimanshuGitCode <singhlink4@gmail.com>
2023-12-29 01:35:43 +0530

    Add Header, Sidebar, and Home components to App.jsx

--- React-Project/src/App.jsx
+++ React-Project/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './header'
+import Sidebar from './Sidebar'
+import Home from './Home'
 
 function App() {
-  
-  return (
-    <>
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
 
-    </>
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  return (
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
   )
 }
 
